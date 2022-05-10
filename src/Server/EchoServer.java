5ec397089f7f0b89689289e9a821b20d7564a22a
Dmitrii <lisp.tnt@gmail.com>
2022-05-10 20:58:22 +0300

    begin primitive server

---
+++ src/Server/EchoServer.java
@@ -0,0 +1,27 @@
+package Server;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class EchoServer {
+    public static void main(String[] args) throws IOException {
+        ServerSocket server = new ServerSocket(8189);
+        System.out.println("Waiting..");
+
+        Socket socket = server.accept();
+        System.out.println("Client conected");
+
+        InputStream is = socket.getInputStream();
+        OutputStream os = socket.getOutputStream();
+
+        Scanner in = new Scanner(is);
+        PrintStream out = new PrintStream(os);
+        out.println("Name?");
+        out.println("hi "+ in.nextLine());
+    }
+}
